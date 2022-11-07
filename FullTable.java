2c722dc439e24e407baae3ee844b66a61e08ef6a
GalyaBryakunova <117297554+GalyaBryakunova@users.noreply.github.com>
2022-11-07 19:45:51 +0400

    Add files via upload

---
+++ FullTable.java
@@ -0,0 +1,12 @@
+public class FullTable {
+    public static void main(String args[]){
+
+        for(int i = 1; i < 11; i++){
+
+            for(int j = 1; j < 11; j++){
+                int result = i * j;
+                System.out.println(i + " * " + j + " = " + result);
+            }
+        }
+    }
+}
